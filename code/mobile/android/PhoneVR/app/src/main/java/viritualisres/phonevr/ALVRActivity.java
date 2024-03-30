3091bcb6cf1720ed8ea4110550192f2fdc67a459
Dominik Wetzel <dominik.wetzel@fh-zwickau.de>
2024-03-30 22:35:41 +0100

    Added option to trigger passthrough by double tapping the headset on the side.

--- code/mobile/android/PhoneVR/app/src/main/java/viritualisres/phonevr/ALVRActivity.java
+++ code/mobile/android/PhoneVR/app/src/main/java/viritualisres/phonevr/ALVRActivity.java
@@ -8,6 +8,10 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.net.Uri;
 import android.opengl.GLSurfaceView;
 import android.os.BatteryManager;
@@ -27,11 +31,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.preference.PreferenceManager;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 public class ALVRActivity extends AppCompatActivity
-        implements PopupMenu.OnMenuItemClickListener, BatteryLevelListener {
+        implements PopupMenu.OnMenuItemClickListener, BatteryLevelListener, SensorEventListener {
 
     static {
         System.loadLibrary("native-lib");
@@ -52,6 +60,169 @@ public class ALVRActivity extends AppCompatActivity
     private BatteryMonitor bMonitor = new BatteryMonitor(this);
 
     private SharedPreferences pref = null;
+
+    private SensorManager mSensorManager;
+
+    private Sensor mAccelerometer;
+
+    private long timestamp = 0;
+
+    private long timestamp_i = 0;
+
+    private long passthrough_delay_ms = 600;
+
+    private float lower_bound = 0.8f;
+
+    private float higher_bound = 4f;
+
+    private List<Float>[] accelStore =
+            new List[] {new ArrayList<>(), new ArrayList<>(), new ArrayList<>()};
+
+    private int halfSize = 0;
+
+    private boolean trigger_set = false;
+
+    private List<Float> sums = new ArrayList<>();
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (timestamp == 0) {
+            timestamp = event.timestamp;
+        }
+
+        if (timestamp_i == 0) {
+            timestamp_i = event.timestamp;
+        }
+
+        boolean initial_full = event.timestamp - timestamp > passthrough_delay_ms * 1000000L;
+        boolean over_i = event.timestamp - timestamp_i > passthrough_delay_ms * 1000000L;
+
+        // gather data for passthrough_delay_ms, we get a data point approx. each 60ms
+        for (int i = 0; i < accelStore.length; ++i) {
+            // derivative, absolute
+            accelStore[i].add(event.values[i]);
+
+            if (initial_full) {
+                // remove the first element to only store the last X elements
+                accelStore[i].remove(0);
+                if (halfSize == 0) {
+                    halfSize = accelStore[0].size() / 2;
+                }
+                // check if half size has at least 3 data points
+                if (halfSize < 4) {
+                    accelStore[i].clear();
+                    timestamp = 0;
+                    halfSize = 0;
+                }
+            }
+        }
+
+        if (halfSize > 0) {
+            // Calculate derivative and absolute values
+            List<Float>[] accelAbs =
+                    new List[] {new ArrayList<>(), new ArrayList<>(), new ArrayList<>()};
+            for (int axis = 0; axis < accelStore.length; ++axis) {
+                for (int i = 1; i < accelStore[axis].size(); ++i) {
+                    accelAbs[axis].add(
+                            Math.abs(accelStore[axis].get(i) - accelStore[axis].get(i - 1)));
+                }
+            }
+
+            // get the mean for each axis and sum them up
+            float[] axis_mean = new float[] {0.0f, 0.0f, 0.0f};
+            for (int i = 0; i < accelAbs.length; ++i) {
+                for (int j = 0; j < accelAbs[i].size(); ++j) {
+                    axis_mean[i] += accelAbs[i].get(j);
+                }
+                axis_mean[i] /= accelAbs[i].size();
+            }
+            float sum_mean = axis_mean[0] + axis_mean[1] + axis_mean[2];
+            sums.add(sum_mean);
+
+            if (sums.size() < accelAbs[0].size()) {
+                return;
+            } else {
+                // only store the last X elements
+                sums.remove(0);
+            }
+
+            if (!over_i) {
+                // if a potential passthrough_change event is found,
+                // check if the device does not move much anymore
+                if (!trigger_set && sum_mean < lower_bound) {
+                    trigger_set = true;
+                    // if passthroughMode not already changed... change it
+                    changePassthroughMode();
+                }
+            } else {
+                trigger_set = false;
+                // here we check for potential passthrough_change events
+                // these may occur on any axis
+                for (List<Float> elem : accelAbs) {
+                    // we look at two halves of the passthrough_delay
+                    // we check:
+                    // - if in the first and second half is a real peak
+                    // - whether the peaks are in the bounds
+                    // - if the summed mean is in the bounds
+                    // - if the first summed mean is below lower_bound
+                    //   (to assure a resting stage before the triggering)
+                    // - if the second maximum is decending under the lower bound until curr value
+                    // If all is fulfilled we consider this a potential passthrough_change event.
+                    // The sum must then decend under lower bound until passthrough_delay
+                    int middle = halfSize - 1;
+                    List<Float> t1 = elem.subList(0, halfSize);
+                    List<Float> t2 = elem.subList(middle, elem.size());
+
+                    float max1 = Collections.max(t1);
+                    float max2 = Collections.max(t2);
+
+                    if (max1 - t1.get(0) < lower_bound
+                            || max1 - t1.get(t1.size() - 1) < lower_bound
+                            || max2 - t2.get(0) < lower_bound
+                            || max2 - t2.get(t2.size() - 1) < lower_bound
+                            || max1 < lower_bound
+                            || max1 > higher_bound
+                            || max2 < lower_bound
+                            || max2 > higher_bound
+                            || sum_mean < lower_bound
+                            || sum_mean > higher_bound) {
+                        continue;
+                    }
+
+                    int end2 = 0;
+                    for (int i = 0; i < t2.size(); ++i) {
+                        if (t2.get(i) == max2) {
+                            end2 = i;
+                        }
+                        if (end2 > 0) {
+                            if (t2.get(i) < lower_bound) {
+                                end2 = i;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (sums.get(0) < Math.max(lower_bound - 0.3f, 0.5f) && t2.get(end2) < max2) {
+                        timestamp_i = event.timestamp;
+                        break;
+                    }
+                }
+            }
+        }
+
+        Log.d(
+                TAG + "-AccSensor",
+                String.format(
+                        (Locale) null,
+                        "%d: [%f, %f, %f]",
+                        event.timestamp,
+                        event.values[0],
+                        event.values[1],
+                        event.values[2]));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {}
 
     public class BatteryMonitor extends BroadcastReceiver {
         private BatteryLevelListener listener;
@@ -96,6 +267,35 @@ public class ALVRActivity extends AppCompatActivity
         }
     }
 
+    private void resumePassthroughDetection() {
+        timestamp = 0;
+        halfSize = 0;
+        if (pref.getBoolean("passthrough_tap", true)) {
+            try {
+                passthrough_delay_ms = Long.parseLong(pref.getString("passthrough_delay", "600"));
+            } catch (Exception e) {
+                passthrough_delay_ms = 600;
+            }
+            try {
+                lower_bound = Float.parseFloat(pref.getString("passthrough_lower", "0.8"));
+            } catch (Exception e) {
+                lower_bound = 0.8f;
+            }
+            try {
+                higher_bound = Float.parseFloat(pref.getString("passthrough_upper", "4"));
+            } catch (Exception e) {
+                higher_bound = 4.f;
+            }
+
+            if (mAccelerometer != null) {
+                mSensorManager.registerListener(
+                        this, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+            }
+        } else {
+            mSensorManager.unregisterListener(this);
+        }
+    }
+
     @Override
     public void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
@@ -133,6 +333,12 @@ public class ALVRActivity extends AppCompatActivity
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         pref = PreferenceManager.getDefaultSharedPreferences(this);
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER_UNCALIBRATED);
+        if (mAccelerometer == null) {
+            mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        }
     }
 
     @Override
@@ -144,6 +350,7 @@ public class ALVRActivity extends AppCompatActivity
     @Override
     protected void onPause() {
         super.onPause();
+        mSensorManager.unregisterListener(this);
         Log.d(TAG, "Pausing ALVR Activity");
         pauseNative();
         glView.onPause();
@@ -167,13 +374,13 @@ public class ALVRActivity extends AppCompatActivity
 
             return;
         }
-
+        resumePassthroughDetection();
         glView.onResume();
         resumeNative();
         bMonitor.startMonitoring(this);
     }
 
-    protected void changeMode(boolean passthrough) {
+    protected void setPassthroughMode(boolean passthrough) {
         if (passthrough) {
             int w = displayWidth / 2;
             int h = displayHeight;
@@ -196,9 +403,16 @@ public class ALVRActivity extends AppCompatActivity
         if (passthrough) {
             cameraHolder.startPreview();
         } else {
-            // cameraHolder.stopPreview();
             cameraHolder.releaseCamera();
         }
+    }
+
+    protected void changePassthroughMode() {
+        boolean pt = !pref.getBoolean("passthrough", false);
+        SharedPreferences.Editor edit = pref.edit();
+        edit.putBoolean("passthrough", pt);
+        edit.apply();
+        setPassthroughMode(pt);
     }
 
     @Override
@@ -267,14 +481,7 @@ public class ALVRActivity extends AppCompatActivity
             return true;
         }
         if (item.getItemId() == R.id.passthrough) {
-            SharedPreferences.Editor edit = pref.edit();
-            boolean pass = true;
-            if (item.isChecked()) {
-                pass = false;
-            }
-            edit.putBoolean("passthrough", pass);
-            edit.apply();
-            changeMode(pass);
+            changePassthroughMode();
             return true;
         }
         return false;
